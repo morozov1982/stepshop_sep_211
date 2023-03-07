f87c6f013f4472e99f080df0e592b6801bce92f2
Александр Морозов <@itstep.loc>
2023-03-07 21:34:30 +0600

    lesson_04 files

--- stepshop/stepshop/urls.py
+++ stepshop/stepshop/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -10,3 +12,6 @@ urlpatterns = [
     path('about/', about, name='about'),
     path('products/', include('mainapp.urls', namespace='products')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
