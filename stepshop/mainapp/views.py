e52e31786fe7fda38e9617dc4d83bb2f236e14f4
Александр Морозов <@itstep.loc>
2023-03-09 21:22:45 +0600

    lesson_05 files

--- stepshop/mainapp/views.py
+++ stepshop/mainapp/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 
+from mainapp.models import Product, ProductCategory
+
 
 def products(request):
-    title = "продукты/каталог"
+    title = "продукты"
+
+    products = Product.objects.all()[:2]
+    categories = ProductCategory.objects.all()
 
     context = {
         'title': title,
+        'products': products,
+        'categories': categories,
     }
 
     # return render(request=request, template_name='products.html', context=context)
