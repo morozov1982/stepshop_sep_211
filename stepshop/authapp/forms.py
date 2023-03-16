2a2b3b5ea1896576947a27158e7a112067be8d01
morozov1982 <morozov1982@gmail.com>
2023-03-16 21:43:19 +0600

    lesson_07 add files

--- stepshop/authapp/forms.py
+++ stepshop/authapp/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from authapp.models import ShopUser
 
@@ -9,8 +10,37 @@ class ShopUserLoginForm(AuthenticationForm):
         fields = ('username', 'password')
 
     def __init__(self, *args, **kwargs):
-        super(ShopUserLoginForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
         # можно назначить стили каждому полю
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+
+class ShopUserRegisterForm(UserCreationForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username',
+                  'first_name',
+                  'password1',
+                  'password2',
+                  'email',
+                  'age',
+                  'avatar')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # можно назначить стили каждому полю
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+    def clean_age(self):
+        data = self.cleaned_data['age']
+
+        if data < 18:
+            raise forms.ValidationError("Вы слишком молоды!")
+
+        return data
--- stepshop/authapp/views.py
+++ stepshop/authapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from authapp.forms import ShopUserLoginForm
+from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm
 
 
 def login(request):
@@ -32,3 +32,23 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+
+def register(request):
+    title = 'регистрация'
+
+    if request.method == "POST":
+        register_form = ShopUserRegisterForm(request.POST, request.FILES)
+
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+    else:
+        register_form = ShopUserRegisterForm()
+
+    context = {
+        'title': title,
+        'register_form': register_form,
+    }
+
+    return render(request, 'auth/register.html', context)
