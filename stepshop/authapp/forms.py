498e292efbb2bb5ece273dd4e1ac243d51961158
morozov1982 <morozov1982@gmail.com>
2023-03-28 20:50:40 +0600

    lesson_08 files

--- stepshop/authapp/forms.py
+++ stepshop/authapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from authapp.models import ShopUser
 
@@ -28,6 +28,33 @@ class ShopUserRegisterForm(UserCreationForm):
                   'email',
                   'age',
                   'avatar')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # можно назначить стили каждому полю
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+    def clean_age(self):
+        data = self.cleaned_data['age']
+
+        if data < 18:
+            raise forms.ValidationError("Вы слишком молоды!")
+
+        return data
+
+
+class ShopUserEditForm(UserChangeForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username',
+                  'first_name',
+                  'email',
+                  'age',
+                  'avatar',
+                  'password')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- stepshop/authapp/views.py
+++ stepshop/authapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm
+from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
 
 
 def login(request):
@@ -52,3 +52,23 @@ def register(request):
     }
 
     return render(request, 'auth/register.html', context)
+
+
+def edit(request):
+    title = 'редактирование'
+
+    if request.method == 'POST':
+        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
+
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('auth:edit'))
+    else:
+        edit_form = ShopUserEditForm(instance=request.user)
+
+    context = {
+        'title': title,
+        'edit_form': edit_form,
+    }
+
+    return render(request, 'auth/edit.html', context)
